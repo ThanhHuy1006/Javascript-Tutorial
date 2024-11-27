f8ae16b7fe47cc19170220f8fa747cee0db1d2fa
Đặng Thanh Huy <86293026+ThanhHuy1006@users.noreply.github.com>
2024-11-27 22:00:43 +0700

    48. Looping Backwards and Loops in Loops

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/todo/loopsInloops.js
@@ -0,0 +1,19 @@
+const thanhhuy=[
+  'dang','thanhhuy',2024-2001,'coder',['Hieu','Thuc','Tung']
+]
+///duyet tu 0,1,2,3,4
+/// gio muon 4,3,,...0
+
+// for(let i=thanhhuy.length-1;i>=0;i--){
+//   console.log(i,thanhhuy[i])
+
+// }
+
+// LOOPS IN LOOPS
+for(let ex=1;ex<4;ex++){
+  console.log(`----Starting exercise ${ex}----`);
+  // for()
+  for(let check=1;check<5;check++){
+    console.log(`   ----let check ${check}----   `);
+  }
+}
