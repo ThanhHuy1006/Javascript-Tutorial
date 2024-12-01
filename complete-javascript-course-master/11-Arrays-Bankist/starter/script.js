77c6792e71f5a7cae0d64c995e5bc16ae49c42c1
Đặng Thanh Huy <86293026+ThanhHuy1006@users.noreply.github.com>
2024-12-01 19:54:39 +0700

    144. the new AT method

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -65,76 +65,89 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
-// sliceMethod
-// systax:
-// array.slice(start, end)
+// /////////////////////////////////////////////////
+// // sliceMethod
+// // systax:
+// // array.slice(start, end)
 
-// Parameters
-// start :
-// Tùy chọn.
-// Vị trí bắt đầu. Mặc định là 0.
-// Số âm chọn từ cuối mảng.
-// end :
-// Tùy chọn.
-// Vị trí kết thúc. Mặc định là phần tử cuối cùng.
-// Số âm chọn từ cuối mảng.
-// Return Value:
-// A new array containing the selected elements.
+// // Parameters
+// // start :
+// // Tùy chọn.
+// // Vị trí bắt đầu. Mặc định là 0.
+// // Số âm chọn từ cuối mảng.
+// // end :
+// // Tùy chọn.
+// // Vị trí kết thúc. Mặc định là phần tử cuối cùng.
+// // Số âm chọn từ cuối mảng.
+// // Return Value:
+// // A new array containing the selected elements.
   
 
 
-let arr=['a','b','c','d','e'];
-console.log(arr.slice());
-// 'a','b','c','d','e'
-console.log(arr.slice(3));
-//mảng bắt đầu từ vị trí số 3
-//'d','e'
+// let arr=['a','b','c','d','e'];
+// console.log(arr.slice());
+// // 'a','b','c','d','e'
+// console.log(arr.slice(3));
+// //mảng bắt đầu từ vị trí số 3
+// //'d','e'
 
 
-console.log(arr.slice(1,3))
-// (2) ['b', 'c']
-console.log(arr.slice(2,3))
+// console.log(arr.slice(1,3))
+// // (2) ['b', 'c']
+// console.log(arr.slice(2,3))
 
-console.log(arr.slice(2,4))
-//['c','d']
-// khong xuat phan tu cuoi cung 
+// console.log(arr.slice(2,4))
+// //['c','d']
+// // khong xuat phan tu cuoi cung 
 
-console.log(arr.slice(-1))
-// ['e']
-console.log(arr.slice(1,-2))
-//['b','c']
+// console.log(arr.slice(-1))
+// // ['e']
+// console.log(arr.slice(1,-2))
+// //['b','c']
 
-//SPLICE
-// giong nhu slice nhung thao tao truc tiep tren mang
-// console.log(arr.splice(3))
+// //SPLICE
+// // giong nhu slice nhung thao tao truc tiep tren mang
+// // console.log(arr.splice(3))
 
+// // console.log(arr)
+// arr.splice(-1)
 // console.log(arr)
-arr.splice(-1)
-console.log(arr)
-// ['a','b','c','d']
-arr.splice(1,2)
-console.log(arr)
-// ['a','d']
-//REVERSE
-///lam thay doi mang ban dau
-arr=['a','b','c','d','e']
-const arr2=['t','h','a','n','h','h','u','y']
-arr2.reverse()
-console.log(arr2)
+// // ['a','b','c','d']
+// arr.splice(1,2)
+// console.log(arr)
+// // ['a','d']
+// //REVERSE
+// ///lam thay doi mang ban dau
+// arr=['a','b','c','d','e']
+// const arr2=['t','h','a','n','h','h','u','y']
+// arr2.reverse()
+// console.log(arr2)
 
-// CONCAT
-let concat1=['h','e','l','l','o'];
-let concat2=['w','o','r','l','d'];
-let concatal=concat1.concat(concat2);
-console.log(concatal)
-///JOIN
-console.log(concatal.join('-'))
+// // CONCAT
+// let concat1=['h','e','l','l','o'];
+// let concat2=['w','o','r','l','d'];
+// let concatal=concat1.concat(concat2);
+// console.log(concatal)
+// ///JOIN
+// console.log(concatal.join('-'))
+
+
+///the new AT method
+const arr=[23,11,64]
+console.log(arr[0]);
+console.log(arr.at(0))
+//get the last element
+console.log(arr[arr.length-1])
+console.log(arr.slice(-1)[0])
+console.log(arr.at(-1))
+///work with string
+console.log('thanhhuy'.at(0))
+console.log('thanhhuy'.at(-1))
