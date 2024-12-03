0742e02ac61398992af00e9ee90cf46baaca9279
Đặng Thanh Huy <86293026+ThanhHuy1006@users.noreply.github.com>
2024-12-03 21:26:16 +0700

    105. Destructuring Objects

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -11,9 +11,6 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  order:function(starterIndex,mainIndex){
-    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
-  },
   openingHours: {
     thu: {
       open: 12,
@@ -28,49 +25,82 @@ const restaurant = {
       close: 24,
     },
   },
+  order:function(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
+  }
 };
 
 
-// 103. Destructuring Arrays
-const arr=[2,3,4]
-const a=arr[0];
-const b=arr[1];
-const c=arr[2];
-///
+// // 103. Destructuring Arrays
+// const arr=[2,3,4]
+// const a=arr[0];
+// const b=arr[1];
+// const c=arr[2];
+// ///
 
-const [x,y,z]=arr;
-console.log(x,y,z);
-// mang ban dau khong bi anh huowng
-console.log(arr)
+// const [x,y,z]=arr;
+// console.log(x,y,z);
+// // mang ban dau khong bi anh huowng
+// console.log(arr)
 
-// const [first,second]=restaurant.categories;
-// console.log(first,second)
-// const [first,,second]=restaurant.categories;tao dau phay de step qua phan tu tiep theo
+// // const [first,second]=restaurant.categories;
+// // console.log(first,second)
+// // const [first,,second]=restaurant.categories;tao dau phay de step qua phan tu tiep theo
 
 
 
-// let [main,,secondary]=restaurant.categories;
-// console.log(main,secondary);
-// //hoan doi main va secondary
-// // const temp=main;
-// // main=secondary;
-// // secondary=temp;
+// // let [main,,secondary]=restaurant.categories;
 // // console.log(main,secondary);
-// /// hoan doi bang destructuring 
-// [main,secondary]=[secondary,main];
-// console.log(main,secondary);
-//vergan,italian
-const [starter,mainCourse]=restaurant.order(2,0)
-console.log(starter,mainCourse)
-//Nested destructuring
-const nested=[2,4,[5,6]];
-// const [,,d]=[2,4,[5,6]];
-// console.log(d);
-const [i, ,[j,k]]=nested;
-console.log(i,j,k);
+// // //hoan doi main va secondary
+// // // const temp=main;
+// // // main=secondary;
+// // // secondary=temp;
+// // // console.log(main,secondary);
+// // /// hoan doi bang destructuring 
+// // [main,secondary]=[secondary,main];
+// // console.log(main,secondary);
+// //vergan,italian
+// const [starter,mainCourse]=restaurant.order(2,0)
+// console.log(starter,mainCourse)
+// //Nested destructuring
+// const nested=[2,4,[5,6]];
+// // const [,,d]=[2,4,[5,6]];
+// // console.log(d);
+// const [i, ,[j,k]]=nested;
+// console.log(i,j,k);
 
 
-//default values
-const [p=1,q=1,r=1]=[8,9];
-console.log(p,q,r)
+// //default values
+// const [p=1,q=1,r=1]=[8,9];
+// console.log(p,q,r)
+
+
+////105. Destructuring Objects
+//ten bien===ten thuoc tinh cua object
+
+const { name,openingHours,categories}=restaurant;
+console.log(name,openingHours,categories)
+const{
+  name:restaurantName,
+  openingHours:hours,
+  categories:tags
+}=restaurant;
+console.log(restaurantName,hours,tags);
+
+
+///default values
+const {menu=[],starterMenu:starter=[]}=restaurant;
+console.log(menu,starter)
+//mutating variables
+let a=111;
+let b=999;
+const obj={a:23,b:7,c:14};
+({a,b}=obj);
+console.log(a,b);
+//nested object
+
+const {fri:{open:o,close:c}}=openingHours;
+console.log(o,c);
+
+
 
