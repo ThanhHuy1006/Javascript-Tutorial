b9eb10ff9f7ac2dd70cc23a3d5ea416f944d6f82
Đặng Thanh Huy <86293026+ThanhHuy1006@users.noreply.github.com>
2024-12-03 20:42:04 +0700

    103. Destructuring Arrays

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -11,7 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
+  order:function(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +29,48 @@ const restaurant = {
     },
   },
 };
+
+
+// 103. Destructuring Arrays
+const arr=[2,3,4]
+const a=arr[0];
+const b=arr[1];
+const c=arr[2];
+///
+
+const [x,y,z]=arr;
+console.log(x,y,z);
+// mang ban dau khong bi anh huowng
+console.log(arr)
+
+// const [first,second]=restaurant.categories;
+// console.log(first,second)
+// const [first,,second]=restaurant.categories;tao dau phay de step qua phan tu tiep theo
+
+
+
+// let [main,,secondary]=restaurant.categories;
+// console.log(main,secondary);
+// //hoan doi main va secondary
+// // const temp=main;
+// // main=secondary;
+// // secondary=temp;
+// // console.log(main,secondary);
+// /// hoan doi bang destructuring 
+// [main,secondary]=[secondary,main];
+// console.log(main,secondary);
+//vergan,italian
+const [starter,mainCourse]=restaurant.order(2,0)
+console.log(starter,mainCourse)
+//Nested destructuring
+const nested=[2,4,[5,6]];
+// const [,,d]=[2,4,[5,6]];
+// console.log(d);
+const [i, ,[j,k]]=nested;
+console.log(i,j,k);
+
+
+//default values
+const [p=1,q=1,r=1]=[8,9];
+console.log(p,q,r)
+
