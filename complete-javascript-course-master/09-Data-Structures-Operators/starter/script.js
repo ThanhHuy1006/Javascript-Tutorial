f133ac505ccfae132d1d0af6a991a4d6903a5194
Đặng Thanh Huy <86293026+ThanhHuy1006@users.noreply.github.com>
2024-12-09 20:43:14 +0700

    113. Enhanced Object Literals-done

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -4,41 +4,74 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 // Data needed for first part of the section
+const openingHours = {
+  // thu: {
+  //   open: 12,
+  //   close: 22,
+  // },
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  // fri: {
+  //   open: 11,
+  //   close: 23,
+  // },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  // sat: {
+  //   open: 0, // Open 24 hours
+  //   close: 24,
+  // },
+  [`day-${2 + 4}`]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (starterIndex, mainIndex) {
+  // openingHours: openingHours,
+  // ES6 ehanced ojbect literals
+  openingHours,
+  // openingHours: {
+  //   thu: {
+  //     open: 12,
+  //     close: 22,
+  //   },
+  //   fri: {
+  //     open: 11,
+  //     close: 23,
+  //   },
+  //   sat: {
+  //     open: 0, // Open 24 hours
+  //     close: 24,
+  //   },
+  // },
+  // order: function (starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
+  oerder(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1},${ing2} and ${ing3}`
     );
   },
-  orderPizza: function (mainIngredient, ...ortherIngredients) {
+  orderPizza(mainIngredient, ...ortherIngredients) {
     console.log(mainIngredient);
     console.log(ortherIngredients);
   },
 };
-
+console.log(restaurant);
 // // 103. Destructuring Arrays
 // const arr=[2,3,4]
 // const a=arr[0];
@@ -294,6 +327,26 @@ const restaurant = {
 // };
 
 // 113. Enhanced Object Literals
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },}
 
 // 122. Working With Strings - Part 1
 // 123. Working With Strings - Part 2
